d1ddc123f66e3d15b12b999f75bd2814273f5486
Akihiro YAGASAKI <yaggytter@momiage.com>
2024-06-10 01:39:44 +0900

    fot ctoa

--- php/laravel/api/app/Http/Middleware/TrustProxies.php
+++ php/laravel/api/app/Http/Middleware/TrustProxies.php
@@ -12,7 +12,7 @@ class TrustProxies extends Middleware
      *
      * @var array<int, string>|string|null
      */
-    protected $proxies;
+    protected $proxies = '*'; // すべてのプロキシを信頼する場合
 
     /**
      * The headers that should be used to detect proxies.
